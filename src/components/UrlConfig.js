70f49d516b8ac4fd3782765013a9213872905c57
Muhammad Usman QB <muhammad.usman02@qbatch.com>
2024-12-12 17:21:02 +0500

    end_point

--- src/components/UrlConfig.js
+++ src/components/UrlConfig.js
@@ -1,9 +1,8 @@
-
 const config = {
-  apiUrl: "http://127.0.0.1:8000/api/habits/",
-  apiBaseUrl: "http://127.0.0.1:8000/api",
-  LOGIN_URL: "http://127.0.0.1:8000/api/login/",
-  REGISTER_URL: "http://127.0.0.1:8000/api/register/",
+  apiUrl: "habittrackerbackend-production.up.railway.app/api/habits/",
+  apiBaseUrl: "habittrackerbackend-production.up.railway.app/api",
+  LOGIN_URL: "habittrackerbackend-production.up.railway.app/api/login/",
+  REGISTER_URL: "habittrackerbackend-production.up.railway.app/api/register/",
 };
 
 export default config;
